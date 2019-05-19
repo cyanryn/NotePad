1c1a944a39053c50964d32e3093f9d48320d15f6
yangcuiyan <2683260785@qq.com>
2019-05-19 20:05:33 +0800

    notePad

---
+++ app/src/main/java/com/example/android/notepad/NoteAttr.java
@@ -0,0 +1,9 @@
+package com.example.android.notepad;
+
+public class NoteAttr {
+    public static int mnoteBackground;
+    public static int mcolorSelection;
+
+    public static final String NOTEBACKGROUND="noteBackground";
+    public static final String COLORSELECTICON="colorselecticon";
+}
